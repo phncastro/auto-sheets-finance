d33394161cbab04d94a4c3e5f81496e1b088f242
Pablo Henrique <phncastro3@gmail.com>
2026-07-07 00:55:17 -0300

    refatoração: removidos comentários e método construtor

--- app/parsers/bradesco.py
+++ app/parsers/bradesco.py
@@ -1,70 +1,47 @@
-# O BANCO É RESPONSÁVEL SOMENTE POR REALIZAR OS MÉTODOS E O PARSE CORRETO DA NOTIFICAÇÃO
-
-'''
-    EX DE NOTIFICAÇÕES: 
-
-    // COMPRA CRÉDITO - APP MENSAGENS
-
-    BRADESCO CARTOES: COMPRA APROVADA CARTAO FINAL 8224 EM
-    29/06/2026 20:16, VALOR R$ 54,99 CONVENIENCIA
-    SÃO PAULO LIMITE DISP R$ 2883,04
-    
-    // PIX ENVIADO - APP BRADESCO
-    
-    Você enviou um Pix de R$ 400,00 para a conta de JOÃO
-    DA SILVA, na Instituição CAIXA ECONOMICA
-    FEDERAL
-
-    // PIX RECEBIDO - APP BRADESCO
-
-    Você recebeu um Pix de R$ 600,00 para a conta de JOÃO
-    DA SILVA, da instituição CAIXA ECONOMICA
-    FEDERAL
-'''
-from app.models import Transacao
+from app.core.transaction import Transacao
 from app.core.enums import TipoTransacao, Banco, RegexTiposBradesco, RegexDadosBradesco
 import re
 
 class BradescoParser:
-    
-    def __init__(self, tipo, valor, descricao, banco):
-        self.tipo = tipo
-        self.valor = valor
-        self.descricao = descricao
-        self.banco = banco
 
     def identificar_tipo(self, transação) -> TipoTransacao:
         
-        if re.search(RegexTiposBradesco.CREDITO, transação):
+        if re.search(RegexTiposBradesco.CREDITO.value, transação):
             return TipoTransacao.CREDITO
-        elif re.search(RegexTiposBradesco.PIX_ENVIADO, transação):
-            return TipoTransacao.PIX_ENVIADO
-        elif re.search(RegexTiposBradesco.PIX_RECEBIDO, transação):
+        elif re.search(RegexTiposBradesco.PIX_ENVIADO.value, transação):
+           return TipoTransacao.PIX_ENVIADO
+        elif re.search(RegexTiposBradesco.PIX_RECEBIDO.value, transação):
             return TipoTransacao.PIX_RECEBIDO
 
-        return 'Error: Tipo não identificado'                                           # /// TRATAR NOTIFICAÇÕES DIFERENTES
+        return None
 
 
     def realizar_parse(self, transação) -> Transacao:
 
-        self.tipo = self.identificar_tipo(transação)
+        tipo = self.identificar_tipo(transação)
 
-        if self.tipo == TipoTransacao.CREDITO:
-            self.valor = re.search(RegexDadosBradesco.VALOR_CARTAO, transação).group()              # /// TRATAR None
-            self.descricao = re.search(RegexDadosBradesco.ESTABELECIMENTO, transação).group()        # /// TRATAR None
-            self.banco = Banco.BRADESCO
+        if tipo == TipoTransacao.CREDITO:
+            valor = re.search(RegexDadosBradesco.VALOR_CARTAO.value, transação).group(1)               # /// TRATAR None
+            descricao = re.search(RegexDadosBradesco.ESTABELECIMENTO.value, transação).group(1)        # /// TRATAR None
+            banco = Banco.BRADESCO
 
-        elif self.tipo == TipoTransacao.PIX_ENVIADO:
-            self.valor = re.search(RegexDadosBradesco.VALOR_PIX, transação).group()                  # /// TRATAR None
-            self.descricao = re.search(RegexDadosBradesco.DESTINATARIO, transação).group()       # /// TRATAR None
-            self.banco = Banco.BRADESCO
+        elif tipo == TipoTransacao.PIX_ENVIADO:
+            valor = re.search(RegexDadosBradesco.VALOR_PIX.value, transação).group(1)                  # /// TRATAR None
+            descricao = re.search(RegexDadosBradesco.DESTINATARIO.value, transação).group(1)           # /// TRATAR None
+            banco = Banco.BRADESCO
 
-        elif self.tipo == TipoTransacao.PIX_RECEBIDO:
-            self.valor = re.search(RegexDadosBradesco.VALOR_PIX, transação).group()                  # /// TRATAR None
-            self.descricao = re.search(RegexDadosBradesco.REMETENTE, transação).group()          # /// TRATAR None
-            self.banco = Banco.BRADESCO
+        elif tipo == TipoTransacao.PIX_RECEBIDO:
+            valor = re.search(RegexDadosBradesco.VALOR_PIX.value, transação).group(1)                  # /// TRATAR None
+            descricao = re.search(RegexDadosBradesco.REMETENTE.value, transação).group(1)              # /// TRATAR None
+            banco = Banco.BRADESCO
 
-        transacao = Transacao(self.tipo, self.valor, self.descricao, self.banco)
+        else:
+            return None
         
-        return transacao
+        return Transacao(
+            tipo,
+            valor,
+            descricao,
+            banco
+        )
 
--- app/parsers/sicoob.py
+++ app/parsers/sicoob.py
@@ -1,85 +1,55 @@
-# O BANCO É RESPONSÁVEL SOMENTE POR REALIZAR OS MÉTODOS E O PARSE CORRETO DA NOTIFICAÇÃO
-
-'''
-    EX DE NOTIFICAÇÕES: 
-
-    // COMPRA CRÉDITO - APP SICOOBCARD
-
-    Sicoobcard informa: compra crédito aprovada com seu cartão
-    Visa final 9920 em 16/09 ás 07:21, valor R$10,00. 
-    Local: SPOTIFY
-
-    // COMPRA CRÉDITO - APP SICOOBCARD
-
-    Sicoobcard informa: compra débito aprovada com seu cartão
-    Visa final 9920 em 16/09 ás 07:21, valor R$10,00. 
-    Local: SPOTIFY
-    
-    // PIX ENVIADO - APP SICOOB
-    
-    Pix de R$500,00 foi enviado para João da Silva, 
-    CPF ***.999.111-**.
-
-    // PIX RECEBIDO - APP SICOOB
-
-    Pix de R$500,00 foi recebido de João da Silva, 
-    CPF ***.999.111-**.
-
-
-'''
-
-from app.models import Transacao
+from app.core.transaction import Transacao
 from app.core.enums import TipoTransacao, Banco, RegexTiposSicoob, RegexDadosSicoob
 import re
 
 class SicoobParser:
-    
-    def __init__(self, tipo, valor, descricao, banco):
-        self.tipo = tipo
-        self.valor = valor
-        self.descricao = descricao
-        self.banco = banco
 
     def identificar_tipo(self, transação):
         
-        if re.search(RegexTiposSicoob.CREDITO, transação):
+        if re.search(RegexTiposSicoob.CREDITO.value, transação):
             return TipoTransacao.CREDITO
-        elif re.search(RegexTiposSicoob.DEBITO, transação):
+        elif re.search(RegexTiposSicoob.DEBITO.value, transação):
             return TipoTransacao.DEBITO
-        elif re.search(RegexTiposSicoob.PIX_ENVIADO, transação):
+        elif re.search(RegexTiposSicoob.PIX_ENVIADO.value, transação):
             return TipoTransacao.PIX_ENVIADO
-        elif re.search(RegexTiposSicoob.PIX_RECEBIDO, transação):
+        elif re.search(RegexTiposSicoob.PIX_RECEBIDO.value, transação):
             return TipoTransacao.PIX_RECEBIDO
 
-        return 'Error: Tipo não identificado'                                          # /// TRATAR NOTIFICAÇÕES DIFERENTES
+        return None
     
 
     def realizar_parse(self, transação):
 
-        self.tipo = self.identificar_tipo()
+        tipo = self.identificar_tipo(transação)
 
-        if self.tipo == TipoTransacao.CREDITO:
-            self.valor = re.search(RegexDadosSicoob.VALOR_CARTAO, transação).group()                      # /// TRATAR None
-            self.descricao = re.search(RegexDadosSicoob.ESTABELECIMENTO, transação).group()        # /// TRATAR None
-            self.banco = Banco.SICOOB
+        if tipo == TipoTransacao.CREDITO:
+            valor = re.search(RegexDadosSicoob.VALOR_CARTAO.value, transação).group(1)               # /// TRATAR None
+            descricao = re.search(RegexDadosSicoob.ESTABELECIMENTO.value, transação).group(1)        # /// TRATAR None
+            banco = Banco.SICOOB
 
-        elif self.tipo == TipoTransacao.DEBITO:
-            self.valor = re.search(RegexDadosSicoob.VALOR_CARTAO, transação).group()                      # /// TRATAR None
-            self.descricao = re.search(RegexDadosSicoob.ESTABELECIMENTO, transação).group()        # /// TRATAR None
-            self.banco = Banco.SICOOB
+        elif tipo == TipoTransacao.DEBITO:
+            valor = re.search(RegexDadosSicoob.VALOR_CARTAO.value, transação).group(1)               # /// TRATAR None
+            descricao = re.search(RegexDadosSicoob.ESTABELECIMENTO.value, transação).group(1)        # /// TRATAR None
+            banco = Banco.SICOOB
 
-        elif self.tipo == TipoTransacao.PIX_ENVIADO:
-            self.valor = re.search(RegexDadosSicoob.VALOR_PIX, transação).group()                  # /// TRATAR None
-            self.descricao = re.search(RegexDadosSicoob.DESTINATARIO, transação).group()           # /// TRATAR None
-            self.banco = Banco.SICOOB
+        elif tipo == TipoTransacao.PIX_ENVIADO:
+            valor = re.search(RegexDadosSicoob.VALOR_PIX.value, transação).group(1)                  # /// TRATAR None
+            descricao = re.search(RegexDadosSicoob.DESTINATARIO.value, transação).group(1)           # /// TRATAR None
+            banco = Banco.SICOOB
 
-        elif self.tipo == TipoTransacao.PIX_RECEBIDO:
-            self.valor = re.search(RegexDadosSicoob.VALOR_PIX, transação).group()                  # /// TRATAR None
-            self.descricao = re.search(RegexDadosSicoob.REMETENTE, transação).group()              # /// TRATAR None
-            self.banco = Banco.SICOOB
+        elif tipo == TipoTransacao.PIX_RECEBIDO:
+            valor = re.search(RegexDadosSicoob.VALOR_PIX.value, transação).group(1)                  # /// TRATAR None
+            descricao = re.search(RegexDadosSicoob.REMETENTE.value, transação).group(1)              # /// TRATAR None
+            banco = Banco.SICOOB
 
-        transacao = Transacao(self.tipo, self.valor, self.descricao, self.banco)
+        else:
+            return None
                 
-        return transacao
+        return Transacao(
+            tipo,
+            valor,
+            descricao,
+            banco
+        )
 
   
