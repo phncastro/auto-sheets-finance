7ce673f0716806dbdf14a9189eeeae03474e2222
Pablo Henrique <phncastro3@gmail.com>
2026-07-09 05:24:30 -0300

    view refatorada

--- app/views.py
+++ app/views.py
@@ -2,42 +2,24 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from app.models import Transacao
+from app.services.transaction_service import TransactionService
+from app.core.notification import Notificacao
+from dataclasses import asdict
 
 @csrf_exempt
 def receber_transacao(request):
     if request.method == "POST":
         data = json.loads(request.body)
-
-        import re
-        regex_valor = r"\s+R\$\s?(\d+(?:\.\d{3})*,\d{2})"
-        regex_valor_bradesco = r"VALOR\s+R\$\s*(\d+(?:\.\d{3})*,\d{2})"
-
-        titulo = data.get('titulo')
-        texto = data.get("texto")
-
-        valor = re.search(regex_valor, texto)
-        if valor:
-            valor = valor.group(1)
-        else:
-            valor = None
-
-        app = data.get("app")
-        
-        transacao = Transacao(
-            texto=texto,
-            banco=app
-            )
-        
-        transacao.save()
-
-        print("=== NOVA TRANSAÇÃO ===")
-        print("Texto:", texto)
-        print("Valor:", valor)
-        print("Banco:", app)
+        notificacao = Notificacao(
+            data['app'],
+            data['titulo'],
+            data['texto']
+        )
+        transacao = TransactionService.processar(notificacao)
 
         return JsonResponse({
             "status": "ok",
-            "recebido": data
+            "transacao": asdict(transacao)
         })
 
     return JsonResponse({"erro": "metodo inválido"})
