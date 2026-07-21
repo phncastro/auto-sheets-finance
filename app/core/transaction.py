c87a24baebef22a7f31381cee5b0ed4be976f409
Pablo Henrique <phncastro3@gmail.com>
2026-07-21 05:13:01 -0300

    refactor: adicionado novos campos para transação

---
+++ app/core/transaction.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from app.core.enums import TipoTransacao, Banco
+from datetime import date
+from decimal import Decimal
+
+@dataclass
+class Transacao:
+    banco: Banco
+    tipo: TipoTransacao
+    valor: Decimal
+    descricao: str
+    data: date = field(default_factory=date.today)
+
+    def linha(self):
+        return [
+            self.data.isoformat(),
+            self.banco.value,
+            self.tipo.value,
+            str(self.valor),
+            self.descricao,
+        ]
--- app/models.py
+++ app/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
+
 class Transacao(models.Model):
     
-    id = models.AutoField(primary_key=True, auto_created=True)
-    tipo = models.CharField(max_length=100)
-    valor = models.FloatField(null=False)
-    descricao = models.CharField(max_length=200, null=False)
-    banco = models.CharField(max_length=100)
+    id = models.AutoField(
+        primary_key=True,
+        auto_created=True
+        )
+    data = models.DateField(
+        auto_now_add=True
+        )
+    banco = models.CharField(
+        max_length=100
+        )
+    tipo = models.CharField(
+        max_length=100
+        )
+    valor = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+        )
+    descricao = models.CharField(
+        max_length=200,
+        null=False
+        )
+    categoria = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+        )
